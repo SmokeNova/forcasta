8c35717f7a0699bf3ef7118c6b8d5fbf32125534
Maaz Imran <ssjbluemaaz@gmail.com>
2023-07-22 15:27:53 +0500

    removed the old localhost api code

--- src/components/FetchWeather.js
+++ src/components/FetchWeather.js
@@ -1,12 +1,10 @@
 
 export async function currentWeather(city) {
-    // const response = await fetch(`http://localhost:5000/api/weather?city=${city}`)
     const response = await fetch(`https://quaint-red-coveralls.cyclic.app/api/weather?city=${city}`)
     const json = await response.json();
     return json;
 }
 export async function forcast(city) {
-    // const response = await fetch(`http://localhost:5000/api/forcast?city=${city}`);
     const response = await fetch(`https://quaint-red-coveralls.cyclic.app/api/forcast?city=${city}`);
     const json = await response.json();
     return json;
