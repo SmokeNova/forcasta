5262f222b3bd37e550dcc86dafca7023de4b9fd8
Maaz Imran <ssjbluemaaz@gmail.com>
2023-07-22 16:48:22 +0500

    did some more changes to the weather card

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -4,7 +4,7 @@ import { faMapMarkerAlt, faCalendar } from '@fortawesome/free-solid-svg-icons'
 
 function Weather({ data }) {
   return (
-    <div className='bg-white rounded-3xl self-center w-[55%] min-w-[285px] sm:w-[24%] px-4 pb-6 pt-8 h-full'>
+    <div className='bg-white rounded-3xl self-center w-[55%] min-w-[280px] md:min-w-[305px] sm:w-[24%] px-4 pb-6 pt-8 h-full'>
       <img src={data.current.condition.icon} alt={data.current.condition.text} className='mb-2 scale-150 mt-4 ml-2' />
       <p className=' text-3xl md:text-7xl mt-8 ml-2 !leading-normal'>{Math.round(data.current.temp_c)}<sup>&deg;C</sup></p>
       <p className='text-sm md:text-base font-bold mt-3 ml-2'>
