427f16795afc93a177b8953555d1f2a957baadde
Maaz Imran <ssjbluemaaz@gmail.com>
2023-07-24 11:14:20 +0500

    changed optional params to url params

--- src/components/FetchWeather.js
+++ src/components/FetchWeather.js
@@ -1,11 +1,11 @@
 
 export async function currentWeather(city) {
-    const response = await fetch(`https://quaint-red-coveralls.cyclic.app/api/weather?city=${city}`)
+    const response = await fetch(`https://quaint-red-coveralls.cyclic.app/api/weather/city=${city}`)
     const json = await response.json();
     return json;
 }
 export async function forcast(city) {
-    const response = await fetch(`https://quaint-red-coveralls.cyclic.app/api/forcast?city=${city}`);
+    const response = await fetch(`https://quaint-red-coveralls.cyclic.app/api/forcast/city=${city}`);
     const json = await response.json();
     return json;
 }
